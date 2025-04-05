80843c60d0831e2b0f4112d253981255ba45fca1
陳奕廷 <chenyiting@chenyitingdebijixingdiannao.local>
2025-04-05 10:18:53 +0800

    修正 GameList.jsx 中的 SVG 圖片路徑問題

--- frontend/src/pages/GameList.jsx
+++ frontend/src/pages/GameList.jsx
@@ -39,7 +39,7 @@ const useStyles = makeStyles((theme) => ({
     left: 0,
     right: 0,
     bottom: 0,
-    background: 'url("/music-pattern.svg") center/cover',
+    background: 'url("/music-theory-academy/music-pattern.svg") center/cover',
     opacity: 0.1,
     zIndex: 0,
   },
